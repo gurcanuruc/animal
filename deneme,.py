8e0e7684ecf762cdebe834198ae21c87d097474d
Emrehan Metin <emrehann17@gmail.com>
2025-03-17 23:32:48 +0300

    Uygulama için bir pencere üzerinde çalışıyorum

--- deneme,.py
+++ deneme,.py
@@ -1,2 +1,26 @@
-print("oldu mu ki? ")
-print("olacak")
+from tkinter import *
+#from tkinter.ttk import *
+
+def button_clicked():
+    print("bunun yerine pencere açılıp fotoğraf seçilecek")
+
+window=Tk()
+
+window.title("Animal Recognizer") 
+window.geometry("800x600")
+window.resizable(width=False, height=False)
+sticker=Label(window , text = "Please import an image to recognize which animal is.",
+              fg="gray",
+              font="Arial 20 bold",
+              )
+tus=Button(window, text ="Choose an image",
+           fg="black",
+           font="Arial 10 bold",
+           cursor = "hand2",
+           anchor= "center",
+           command=button_clicked,
+           )
+sticker.pack()
+tus.pack()
+
+window.mainloop()
