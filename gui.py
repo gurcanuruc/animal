9b7fa651b11187f4855561855498b0057d936ced
Emrehan Metin <emrehann17@gmail.com>
2025-03-18 10:33:29 +0300

    bığ

--- gui.py
+++ gui.py
@@ -9,18 +9,22 @@ window=Tk()
 window.title("Animal Recognizer") 
 window.geometry("800x600")
 window.resizable(width=False, height=False)
+window.configure(bg="#262626")
 sticker=Label(window , text = "Please import an image to recognize which animal is.",
               fg="gray",
+              bg ="#262626",
               font="Arial 20 bold",
               )
-tus=Button(window, text ="Choose an image",
+
+btn=Button(window, text ="Choose an image",
            fg="black",
-           font="Arial 10 bold",
+           font="Arial 20 bold",
+           bg ="#424242",
            cursor = "hand2",
            anchor= "center",
            command=button_clicked,
            )
-sticker.pack()
-tus.pack()
+sticker.pack(pady="20")
+btn.pack(pady="100")
 
 window.mainloop()
